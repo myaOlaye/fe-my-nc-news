c5d838d0764d07ffd21f6fe4a1e7fe600371b54d
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2024-12-16 15:33:48 +0000

    Add loading state to Article

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -5,20 +5,30 @@ import { useEffect, useState } from "react";
 export const Article = () => {
   const [article, setArticle] = useState({});
   const { article_id, title } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     getArticle(article_id, title).then(({ article }) => {
       setArticle(article);
+      setIsLoading(false);
     });
   }, []);
 
   return (
     <section>
-      <h2>{article.title}</h2>
-      <p>{article.author}</p>
-      <p>{article.created_at}</p>
-      <p>Topic: {article.topic}</p>
-      <p>{article.body}</p>
+      {isLoading ? (
+        <p>Loading</p>
+      ) : (
+        <>
+          {" "}
+          <h2>{article.title}</h2>
+          <p>{article.author}</p>
+          <p>{article.created_at}</p>
+          <p>Topic: {article.topic}</p>
+          <p>{article.body}</p>{" "}
+        </>
+      )}
     </section>
   );
 };
