22b84e1732f98c5ac613d24db64bbd8ecc62272d
myaOlaye <136443656+myaOlaye@users.noreply.github.com>
2024-12-17 12:25:10 +0000

    Added link back to home in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
   return (
     <header>
-      <h1>News Application</h1>
+      <h1>
+        <Link to="/">News Application </Link>
+      </h1>
     </header>
   );
 };
